3085f938bf42f2309c998c3e57a61455a9ad88d3
guohongjian <2844735407@qq.com>
2018-11-30 17:19:42 +0800

    react-router-dom练习

---
+++ src/router/AppRouter.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import Time from '../components/Time'
+
+const Index = () => (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+const About = () => <h2>About</h2>
+const User = () => <h2>User</h2>
+const Topic = ({ match }) => <h3>Requested Param: { match.params.id }</h3>
+const Topics = ({ match }) => (
+    <div>
+        <h2>Topics</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/components`}>Components</Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>Props v .State</Link>
+            </li>
+        </ul>
+
+        <Route path={`${match.path}/:id`} compontent={Topic}/>
+        <Route
+            exact
+            path={match.path}
+            render={() => <h3>Please select a topic.</h3>}
+        />
+    </div>
+)
+
+const AppRouter = () => (
+    <Router>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to = '/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to = '/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to = '/user'>Users</Link>
+                    </li>
+                    <li>
+                        <Link to = '/topics'>Topics</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/about" component={About} />
+            <Route path="/user" component={User} />
+            <Route path="/topics" component={Topics} />
+
+        </div>
+    </Router>
+)
+
+export default AppRouter
